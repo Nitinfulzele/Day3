2d2cc5beeaf7e58e29007eb12f75099efeb024d1
Nitinfulzele <nitinfulzele99@gmail.com>
2022-09-30 23:45:10 +0530

    Employee Wage Day 3

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,13 @@
+package EmployeeWageComputation;
+
+
+
+public class EmployeeWageComputation {
+	
+	
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation ");
+	    
+	}
+}
