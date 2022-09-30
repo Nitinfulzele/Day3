d304852e38310504fa9398f551fa9b43e0655795
Nitinfulzele <nitinfulzele99@gmail.com>
2022-10-01 00:17:37 +0530

    Added Employee Part Time

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,6 +7,7 @@ public class EmployeeWageComputation {
 	
 	static int wagePerHour = 20;
 	static int fullDayHour = 8;
+	static int partTimeHour = 8;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation ");
@@ -22,7 +23,11 @@ public class EmployeeWageComputation {
 			System.out.println("Employee is Absent");
 		}
 		int dailyWage = 0;
+		
 		dailyWage = fullDayHour * wagePerHour;
 		System.out.println("Daily wage of Employee is " + dailyWage);
+	    
+		dailyWage = partTimeHour * wagePerHour;
+		System.out.println("Employee is Part time");
 	}
 }
